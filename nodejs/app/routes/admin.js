39691f56312edb6249571ae23e483cf29d254e97
Marcus Santos <marcus817@gmail.com>
2017-06-28 07:59:27 -0300

    adding input in form and implementing validation
    create  new fields in form and adding validation logic

--- nodejs/app/routes/admin.js
+++ nodejs/app/routes/admin.js
@@ -7,6 +7,18 @@ module.exports = function(app){
 	app.post('/noticias/salvar', function(req,res){
 		let noticia = req.body;
 		
+		req.assert('titulo','').notEmpty();
+		req.assert('noticia','').len(10,100);
+		req.assert('resumo','').notEmpty();
+		req.assert('autor','').notEmpty();
+		req.assert('data_noticia','').isDate({format: 'YYYY-MM-DD'});
+
+		let errors = req.validationErrors();
+
+		if (errors){
+			res.redirect('/novanoticia');
+			return;
+		}
 		var connection = app.config.dbConnection();
 		var noticiasModel = new app.app.models.noticiasDAO(connection);
 		
