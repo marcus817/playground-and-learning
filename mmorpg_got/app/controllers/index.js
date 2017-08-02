01b806e67dbde1b745128d13bef476359793ec35
Marcus Santos <marcus817@gmail.com>
2017-08-02 16:20:43 -0300

    Assinatura
    modulo de assinatura

--- mmorpg_got/app/controllers/index.js
+++ mmorpg_got/app/controllers/index.js
@@ -1,3 +1,30 @@
 module.exports.index = function(application, req, res){
-	res.render('index')
+	res.render('index', {validacao:[]});
+}
+
+module.exports.assinatura = function(application, req, res){
+	res.render('cadastrooutass', {validacao:[]});
+}
+
+module.exports.assinar = function(application, req, res){
+	var dados = req.body;
+	console.log(dados);
+
+	res.render('meiralins',{dados});
+}
+
+
+module.exports.autenticar = function(application, req, res){
+	let dadosForm = req.body;
+	req.assert('usuario', 'Usuario não pode ser vazio').notEmpty();
+	req.assert('senha', 'Senha não pode ser vazia').notEmpty();
+
+	var erros = req.validationErrors();
+
+	if(erros){
+		res.render("index", {validacao:erros});
+		return;
+	}
+
+	res.send('Criar Sessão');
 }
--- mmorpg_got/app/routes/index.js
+++ mmorpg_got/app/routes/index.js
@@ -2,4 +2,16 @@ module.exports = function(application){
 	application.get('/', function(req, res){
 		application.app.controllers.index.index(application, req, res);
 	});
+
+	application.post('/autenticar', function(req, res){
+		application.app.controllers.index.autenticar(application, req, res);
+	});
+
+	application.get('/assinatura', function(req, res){
+		application.app.controllers.index.assinatura(application, req, res);
+	});
+
+	application.post('/assinar', function(req, res){
+		application.app.controllers.index.assinar(application, req, res);
+	});
 }
