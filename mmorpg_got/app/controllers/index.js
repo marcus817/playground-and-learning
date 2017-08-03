cde1aa739333786413efb60faaf4d8d6d20ea478
Marcus Santos <marcus817@gmail.com>
2017-08-03 13:35:41 -0300

    signeture generator
    Criador de assinatura ADTSA

--- mmorpg_got/app/controllers/index.js
+++ mmorpg_got/app/controllers/index.js
@@ -1,3 +1,7 @@
+var htmlConvert = require('html-convert');
+var fs = require('fs');
+
+
 module.exports.index = function(application, req, res){
 	res.render('index', {validacao:[]});
 }
@@ -8,11 +12,52 @@ module.exports.assinatura = function(application, req, res){
 
 module.exports.assinar = function(application, req, res){
 	var dados = req.body;
-	console.log(dados);
+	console.log(req.body.empresa);
 
-	res.render('meiralins',{dados});
+
+	switch(dados.empresa){
+		case "1":
+		console.log("ADTSA");
+		res.render('ADTSA',{dados});
+		break;
+		case "2":
+		console.log("REGENCE");
+		res.render('Regence',{dados});
+		break;
+		case "3":
+		console.log("MEIRALINS");
+		res.render('meiralins',{dados});
+		break;
+		case "4":
+		console.log("NOVO MUNDO");
+		res.render('NovoMundo',{dados});
+		break;
+		case "5":
+		console.log("PIGALLE");
+		res.render('PigalleCitroen',{dados});
+		break;
+		case "6":
+		console.log("PGLE");
+		res.render('PigallePeugeot',{dados});
+		break;
+		case "7":
+		console.log("SILCAR");
+		res.render('Silcar',{dados});
+		break;
+		case "8":
+		console.log("WELLE");
+		res.render('Welle',{dados});
+		break;
+
+	}
+	
+	
 }
 
+
+module.exports.geraPNG = function(application, req, res){
+	
+}
 
 module.exports.autenticar = function(application, req, res){
 	let dadosForm = req.body;
--- mmorpg_got/app/routes/index.js
+++ mmorpg_got/app/routes/index.js
@@ -14,4 +14,11 @@ module.exports = function(application){
 	application.post('/assinar', function(req, res){
 		application.app.controllers.index.assinar(application, req, res);
 	});
+
+	application.post('/gerapng', function(req, res){
+		//application.app.controllers.index.geraPNG(application, req, res);
+		console.log("Chegou Aqui");
+		console.log(req);
+		console.log("Chegou Aqui");
+	})
 }
